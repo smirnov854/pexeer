524fa823423957577a7d11c82d995691b03ebbdd
eeu <smirnov_e87@mail.ru>
2022-01-30 23:42:04 +0300

    new version 1.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
         Schema::defaultStringLength(191);
 
         Validator::extend('strong_pass', function($attribute, $value, $parameters, $validator) {
